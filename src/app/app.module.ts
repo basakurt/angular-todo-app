f30b6fcf7b7d981b9705c92baaddfb39ea80300e
basakurt <basak.kurt@accenture.com>
2020-04-19 22:02:20 +0300

    Add todo list item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component'
+import { TodoListItemComponent } from './todo-list/todo-list-item/todo-list-item.component';
+import { TodoListAddItemComponent } from './todo-list/todo-list-add-item/todo-list-add-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    TodoListItemComponent,
+    TodoListAddItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/todo-list/todo-list-add-item/todo-list-add-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-todo-list-add-item',
+    templateUrl: './todo-list-add-item.component.html',
+    styleUrls: ['./todo-list-add-item.component.scss']
+})
+export class TodoListAddItemComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
